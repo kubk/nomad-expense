f769715c7f7ab6b682329165d456e8e56e13543c
Gorbachev Egor <7gorbachevm@gmail.com>
2025-08-21 20:47:50 +0700

    ID based auth
    This commit was filtered by https://github.com/kubk/git-filter
    Some files were excluded, so this commit may appear empty or incomplete.

--- packages/api/src/lib/cloudflare/cors.ts
+++ packages/api/src/lib/cloudflare/cors.ts
@@ -2,7 +2,10 @@ export const addCorsHeaders = (response?: Response): Response => {
   const newHeaders = new Headers(response?.headers);
   newHeaders.set("Access-Control-Allow-Origin", "*");
   newHeaders.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  newHeaders.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  newHeaders.set(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, x-user-id",
+  );
   newHeaders.set("Access-Control-Max-Age", "604800");
 
   return new Response(response?.body, {
