fd2c0c345183086df07f1c49e1ae5d1f54550717
Gorbachev Egor <7gorbachevm@gmail.com>
2025-08-24 13:58:38 +0700

    All monthly breakdown clickable
    This commit was filtered by https://github.com/kubk/git-filter
    Some files were excluded, so this commit may appear empty or incomplete.

--- packages/frontend/src/pages/overview/monthly-breakdown-overview.tsx
+++ packages/frontend/src/pages/overview/monthly-breakdown-overview.tsx
@@ -5,31 +5,37 @@ import { render } from "typesafe-routes";
 import { routes } from "../../routes";
 import { MonthlyChart } from "./monthly-chart";
 import { useAccountIds } from "@/shared/hooks/use-account-ids";
+import { useLocation } from "wouter";
 
 export function MonthlyBreakdownOverview() {
   const accountIds = useAccountIds();
+  const [, navigate] = useLocation();
 
   return (
     <div className="px-4 -mt-14">
-      <Card className="shadow border-0 gap-0 py-4">
+      <Card
+        className="shadow border-0 gap-0 py-4 cursor-pointer"
+        onClick={() =>
+          navigate(
+            render(routes.monthlyBreakdownFull, {
+              query: {
+                filters: {
+                  accounts: accountIds,
+                  date: { type: "months", value: 3 },
+                },
+              },
+              path: {},
+            }),
+          )
+        }
+      >
         <CardHeader className="pb-3">
           <div className="flex justify-between items-center">
             <CardTitle className="text-base">Monthly breakdown</CardTitle>
-            <Link
-              href={render(routes.monthlyBreakdownFull, {
-                query: {
-                  filters: {
-                    accounts: accountIds,
-                    date: { type: "months", value: 3 },
-                  },
-                },
-                path: {},
-              })}
-              className="text-primary/70 -mr-3 inline-flex items-center text-sm font-medium"
-            >
+            <span className="text-primary/70 -mr-3 inline-flex items-center text-sm font-medium">
               View all
               <ChevronRightIcon className="w-3 h-3 ml-1" />
-            </Link>
+            </span>
           </div>
         </CardHeader>
         <CardContent className="pr-4 pt-1">
