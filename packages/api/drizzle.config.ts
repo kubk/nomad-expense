71bcc10377e879fad3bd142fdaad518d0907ed54
Gorbachev Egor <7gorbachevm@gmail.com>
2025-09-07 17:54:06 +0700

    Use a package to access D1 locally
    This commit was filtered by https://github.com/kubk/git-filter
    Some files were excluded, so this commit may appear empty or incomplete.

--- packages/api/drizzle.config.ts
+++ packages/api/drizzle.config.ts
@@ -1,27 +1,10 @@
 import { defineConfig } from "drizzle-kit";
-import fs from "fs";
-import path from "path";
 import { getEnv, setEnv } from "./src/services/env";
+import { D1Helper } from "@nerdfolio/drizzle-d1-helpers";
 
 setEnv(process.env);
 
-function getLocalD1DB() {
-  try {
-    const basePath = path.resolve(".wrangler");
-    const dbFile = fs
-      .readdirSync(basePath, { encoding: "utf-8", recursive: true })
-      .find((f) => f.endsWith(".sqlite"));
-
-    if (!dbFile) {
-      throw new Error(`.sqlite file not found in ${basePath}`);
-    }
-
-    const url = path.resolve(basePath, dbFile);
-    return url;
-  } catch (err) {
-    console.log(`Error  ${err.message}`);
-  }
-}
+const dbHelper = D1Helper.get("DB");
 
 export default defineConfig({
   dialect: "sqlite",
@@ -38,7 +21,7 @@ export default defineConfig({
       }
     : {
         dbCredentials: {
-          url: getLocalD1DB(),
+          url: dbHelper.sqliteLocalFileCredentials.url,
         },
       }),
 });
