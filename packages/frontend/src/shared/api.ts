38d34cb74d25040576497603f2d3a1d0465ea412
Gorbachev Egor <7gorbachevm@gmail.com>
2025-09-12 21:26:35 +0700

    Deploy FE

--- packages/frontend/src/shared/api.ts
+++ packages/frontend/src/shared/api.ts
@@ -8,8 +8,8 @@ import { getAuthToken } from "./auth-token";
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 1000 * 60 * 5, // 5 minutes
-      gcTime: 1000 * 60 * 10, // 10 minutes
+      staleTime: 1000 * 60 * 5,
+      gcTime: 1000 * 60 * 10,
     },
   },
 });
