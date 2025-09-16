2cdcb15da72132ac3bd9d5e6f4074dee4c3ed40e
Gorbachev Egor <7gorbachevm@gmail.com>
2025-09-16 13:00:04 +0700

    ts

--- packages/frontend/src/pages/overview/overview-screen.tsx
+++ packages/frontend/src/pages/overview/overview-screen.tsx
@@ -2,15 +2,17 @@ import { RecentTransactionsOverview } from "./recent-transactions-overview";
 import { OverviewHeader } from "./overview-header";
 import { MonthlyBreakdownOverview } from "./monthly-breakdown-overview";
 import { AddTransactionFab } from "./add-transaction-fab";
-import { PlainPage } from "../shared/page";
+import { cn } from "@/lib/utils";
 
 export function OverviewScreen() {
   return (
-    <PlainPage className="bg-muted/100 pb-30 overflow-auto">
+    <div
+      className={cn("flex flex-col h-full bg-muted/100 pb-30 overflow-auto")}
+    >
       <OverviewHeader />
       <MonthlyBreakdownOverview />
       <RecentTransactionsOverview />
       <AddTransactionFab />
-    </PlainPage>
+    </div>
   );
 }
