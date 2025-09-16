d8c9f9a550e7a0da42c8bacfe7a7158c300cd1ba
Gorbachev Egor <7gorbachevm@gmail.com>
2025-09-16 10:29:42 +0700

    Try safe area

--- packages/frontend/src/shared/telegram.ts
+++ packages/frontend/src/shared/telegram.ts
@@ -9,3 +9,20 @@ export function isRunningWithinTelegram() {
 export function getInitData() {
   return getWebApp()?.initData ?? null;
 }
+
+export function getSafeAreaInset() {
+  return getWebApp()?.safeAreaInset ?? { top: 0, bottom: 0, left: 0, right: 0 };
+}
+
+export function getContentSafeAreaInset() {
+  return getWebApp()?.contentSafeAreaInset ?? { top: 0, bottom: 0, left: 0, right: 0 };
+}
+
+export function initializeTma() {
+  if (!isRunningWithinTelegram()) return;
+  const webApp = getWebApp();
+  if (!webApp) return;
+
+  webApp.ready();
+  webApp.disableVerticalSwipes();
+}
