0973facfb615ee19347ea8cff728d3599ad06a33
Gorbachev Egor <7gorbachevm@gmail.com>
2025-09-16 13:29:35 +0700

    Sync TG header color
    This commit was filtered by https://github.com/kubk/git-filter
    Some files were excluded, so this commit may appear empty or incomplete.

---
+++ packages/frontend/src/shared/use-tg-header-color-sync.ts
@@ -0,0 +1,37 @@
+import { useTheme } from "@/pages/shared/theme-provider";
+import { useEffect } from "react";
+import { getWebApp } from "./telegram";
+
+export function useTgHeaderColorMainSync() {
+  const { theme } = useTheme();
+  useEffect(() => {
+    const webApp = getWebApp();
+    if (!webApp) return;
+    if (theme === "dark") {
+      webApp.setHeaderColor("#262626");
+    } else {
+      webApp.setHeaderColor("#171717");
+    }
+  }, [theme]);
+}
+
+export function useTgHeaderColorSync(bg: "primary" | "secondary") {
+  const { theme } = useTheme();
+  useEffect(() => {
+    const webApp = getWebApp();
+    if (!webApp) return;
+    if (theme === "dark") {
+      if (bg === "primary") {
+        webApp.setHeaderColor("#0a0a0a");
+      } else {
+        webApp.setHeaderColor("#262626");
+      }
+    } else {
+      if (bg === "primary") {
+        webApp.setHeaderColor("#ffffff");
+      } else {
+        webApp.setHeaderColor("#f5f5f5");
+      }
+    }
+  }, [bg, theme]);
+}
