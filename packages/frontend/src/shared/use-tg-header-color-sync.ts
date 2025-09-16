68c5ff786a3f501eb0d7d99bf086da16c8d37c84
Gorbachev Egor <7gorbachevm@gmail.com>
2025-09-16 17:11:30 +0700

    Try fixing header

--- packages/frontend/src/shared/use-tg-header-color-sync.ts
+++ packages/frontend/src/shared/use-tg-header-color-sync.ts
@@ -1,24 +1,38 @@
 import { useTheme } from "@/pages/shared/theme-provider";
 import { useEffect } from "react";
 import { getWebApp } from "./telegram";
+import { useRouter } from "@/shared/stacked-router/router";
 
-export function useTgHeaderColorSync(bg: "primary" | "secondary") {
+export function useTgHeaderColorSync() {
   const { theme } = useTheme();
+  const { currentRoute } = useRouter();
+
+  const isPrimary =
+    currentRoute?.type === "settings" ||
+    currentRoute?.type === "family" ||
+    currentRoute?.type === "accountForm" ||
+    currentRoute?.type === "transactionForm";
+
   useEffect(() => {
     const webApp = getWebApp();
     if (!webApp) return;
+
     if (theme === "dark") {
-      if (bg === "primary") {
+      if (isPrimary) {
+        // console.log('primary dark')
         webApp.setHeaderColor("#0a0a0a");
       } else {
+        // console.log('not primary dark')
         webApp.setHeaderColor("#262626");
       }
     } else {
-      if (bg === "primary") {
+      if (isPrimary) {
+        // console.log('primary light')
         webApp.setHeaderColor("#ffffff");
       } else {
+        // console.log('not primary light')
         webApp.setHeaderColor("#f5f5f5");
       }
     }
-  }, [bg, theme]);
+  }, [theme, isPrimary]);
 }
