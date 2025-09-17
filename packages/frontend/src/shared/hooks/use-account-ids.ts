64e0a51979f3950d578dea3d14d43387ea2a2374
Gorbachev Egor <7gorbachevm@gmail.com>
2025-09-17 11:09:38 +0700

    Try http link

--- packages/frontend/src/shared/hooks/use-account-ids.ts
+++ packages/frontend/src/shared/hooks/use-account-ids.ts
@@ -2,7 +2,10 @@ import { trpc } from "@/shared/api";
 import { useQuery } from "@tanstack/react-query";
 
 export function useAccountIds() {
-  const { data: accounts } = useQuery(trpc.accounts.list.queryOptions());
+  const { data: accounts } = useQuery({
+    ...trpc.accounts.list.queryOptions(),
+    select: (accounts) => accounts.map((a) => a.id),
+  });
 
-  return accounts?.map((account) => account.id) ?? [];
+  return accounts ?? [];
 }
