3393f850037fb6a342b6f030c0867c5785dc6bd6
Gorbachev Egor <7gorbachevm@gmail.com>
2025-09-18 12:15:44 +0700

    f
    This commit was filtered by https://github.com/kubk/git-filter
    Some files were excluded, so this commit may appear empty or incomplete.

--- packages/api/src/services/money/money.ts
+++ packages/api/src/services/money/money.ts
@@ -1,35 +1,29 @@
 import { convert, SupportedCurrency } from "./currency-converter";
 
 export type MoneyFull = {
-  type: "full";
   amountCents: number;
   currency: SupportedCurrency;
   baseAmountCents: number;
   baseCurrency: SupportedCurrency;
 };
 
-export function createMoneyFull({
-  amount,
-  currency,
-}: {
-  // Human amount - amount in user's currency, e.g. 20
-  // Cents amount - amount in currency's smallest unit, e.g. 2000
-  amount: { amountHuman: number } | { amountCents: number };
-  currency: SupportedCurrency;
-}): MoneyFull {
+export function createMoneyFull(
+  params:
+    | { amountHuman: number; currency: SupportedCurrency }
+    | { amountCents: number; currency: SupportedCurrency }
+): MoneyFull {
   // For now we keep USD hardcoded until we implement proper currency conversion
   const baseCurrency = "USD";
 
   const amountCents =
-    "amountHuman" in amount
-      ? Math.round(amount.amountHuman * 100)
-      : amount.amountCents;
+    "amountHuman" in params
+      ? Math.round(params.amountHuman * 100)
+      : params.amountCents;
 
   return {
-    type: "full" as const,
     amountCents,
-    currency,
+    currency: params.currency,
+    baseAmountCents: convert(amountCents, params.currency, baseCurrency),
     baseCurrency,
-    baseAmountCents: convert(amountCents, currency, baseCurrency),
   };
 }
