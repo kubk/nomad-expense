f164699500ae7390b6e17c8a1c0a6a315107f297
Gorbachev Egor <7gorbachevm@gmail.com>
2025-09-18 15:36:40 +0700

    Rules
    This commit was filtered by https://github.com/kubk/git-filter
    Some files were excluded, so this commit may appear empty or incomplete.

--- packages/api/src/db/db-types.ts
+++ packages/api/src/db/db-types.ts
@@ -1,4 +1,12 @@
-import { accountTable } from "./schema";
+import {
+  accountTable,
+  transactionImportRuleTable,
+  transactionTable,
+} from "./schema";
 import { InferSelectModel } from "drizzle-orm";
 
 export type AccountSelect = InferSelectModel<typeof accountTable>;
+export type TransactionImportRule = InferSelectModel<
+  typeof transactionImportRuleTable
+>;
+export type Transaction = InferSelectModel<typeof transactionTable>;
