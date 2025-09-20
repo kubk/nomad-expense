ec07061955c45de24a0ec3dd915a3b470fae792a
Gorbachev Egor <7gorbachevm@gmail.com>
2025-09-20 22:44:11 +0700

    Trigger FE
    This commit was filtered by https://github.com/kubk/git-filter
    Some files were excluded, so this commit may appear empty or incomplete.

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -11,7 +11,7 @@ const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: 1000 * 60 * 5, // 5 minutes
-      gcTime: 1000 * 60 * 10, // 10 minutes
+      gcTime: 1000 * 60 * 10, //10 minutes
     },
   },
 });
