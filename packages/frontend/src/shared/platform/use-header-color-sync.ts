52c0f9ef8ca8160a5b7e70e40264c6072eaa6c88
Gorbachev Egor <7gorbachevm@gmail.com>
2025-09-26 15:39:21 +0700

    Simplify header colors

--- packages/frontend/src/shared/platform/use-header-color-sync.ts
+++ packages/frontend/src/shared/platform/use-header-color-sync.ts
@@ -1,27 +1,14 @@
 import { useTheme } from "@/pages/widgets/theme-provider";
 import { useEffect } from "react";
-import { useRouter } from "@/shared/stacked-router/router";
-import { isFormRoute } from "@/shared/stacked-router/routes";
 import { platform } from "./platforms";
 
 export function useHeaderColorSync() {
   const { theme } = useTheme();
-  const { currentRoute } = useRouter();
-  const isForm = isFormRoute(currentRoute);
-
   useEffect(() => {
     if (theme === "dark") {
-      if (isForm) {
-        platform.syncHeader("#0a0a0a");
-      } else {
-        platform.syncHeader("#262626");
-      }
+      platform.syncHeader("#262626");
     } else {
-      if (isForm) {
-        platform.syncHeader("#ffffff");
-      } else {
-        platform.syncHeader("#f5f5f5");
-      }
+      platform.syncHeader("#f5f5f5");
     }
-  }, [theme, isForm]);
+  }, [theme]);
 }
