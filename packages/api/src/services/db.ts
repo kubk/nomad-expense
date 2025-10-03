6d934a05316db58beb8138abec20879a987a6398
Gorbachev Egor <7gorbachevm@gmail.com>
2025-10-03 13:14:28 +0700

    PG configuration
    This commit was filtered by https://github.com/kubk/git-filter
    Some files were excluded, so this commit may appear empty or incomplete.

--- packages/api/src/services/db.ts
+++ packages/api/src/services/db.ts
@@ -7,9 +7,6 @@ export function getDb() {
   const client = postgres(getEnv().DB_URL, {
     prepare: false,
     fetch_types: false,
-    max: 7,
-    idle_timeout: 0,
-    max_lifetime: 0,
   });
   return drizzle(client, { schema });
 }
